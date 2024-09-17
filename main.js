235e72bc1e754e15de38b451867c733e99fd2aec
mephaline <suaramos88@gmail.com>
2024-09-17 02:05:42 -0500

    completed assignment

--- main.js
+++ main.js
@@ -3,7 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+const hearts = document.querySelectorAll(".like-glyph");
+for (let i = 0; i < hearts.length; i++) {
+  console.log(hearts[i].textContent);
+  hearts[i].addEventListener("click", (e) => {
+    e.target.textContent === FULL_HEART
+      ? ((e.target.textContent = EMPTY_HEART),
+        e.target.classList.remove("activated-heart"))
+      : ((e.target.textContent = FULL_HEART),
+        e.target.classList.add("activated-heart"));
+    console.log(e.target.parentNode);
+  });
+}
+function respFromServer() {
+  // a response from the server
+  document.getElementById("modal").classList.add("hidden");
+  fetch(" http://localhost:3000/hearts")
+    .then((res) => console.log(res))
+    .catch(() => {
+      
+      document.getElementById("modal").classList.remove("hidden");
+      setTimeout(
+        () => document.getElementById("modal").classList.add("hidden"),
+        3000
+      );
+    });
+}
+respFromServer();
 
 
 
